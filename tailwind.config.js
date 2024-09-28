266ed098b40aa4ca1d8565a0815392684cf23a6c
Dhruv kumar maheshwari <dhruvmaheswari23@gmail.com>
2024-09-28 23:38:57 +0530

    question ejs ui component and color theme changed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      boxShadow: {
+        'neon': '0 0 10px rgba(126, 48, 225, 0.8)'
+      }, 
+    },
+  },
+  plugins: [],
+}
+
