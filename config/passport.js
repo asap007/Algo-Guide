93f93e7356b4e89f57ea7618bd4efb2d402a5997
asap007 <anshulofficial1202@gmail.com>
2024-10-03 00:39:32 +0530

    changed callback for deployement

--- config/passport.js
+++ config/passport.js
@@ -30,7 +30,7 @@ module.exports = function(passport) {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback"
+    callbackURL: "https://algo-guide.onrender.com/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
